cc54193c49c0e804d81b87fd25cd84d471d10b21
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2022-03-13 18:07:28 +0100

    created single menu page html and css

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
 Route::get('/menu', function () {
     return view('menu/index');
 });
+Route::get('/menu/{slug}', function () {
+    return view('menu/single-menu');
+});
 Route::get('/waitlist', function () {
     return view('pages/waitlist');
 });
