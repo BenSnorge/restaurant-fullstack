32dae1016acbb726189197d13bdbbc3def2f4e50
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2022-03-21 12:14:18 +0100

    Able to see all food items and edit them

--- resources/views/admin/food-categories/all.blade.php
+++ resources/views/admin/food-categories/all.blade.php
@@ -50,13 +50,13 @@
                     <td>{{ $category->title }}</td>
                     <td>{{ date('m/d/Y', strtotime($category->updated_at)) }}</td>
                     <td><a href="/admin/food-categories/{{ $category->id }}/edit"><i class="far fa-edit"></i></a></td>
-                    <td>{{-- <a href="/admin/users/{{ $category->id }}/delete" onclick="if(! confirm('Are you sure you want to delete category?')) { return false; }"><i class="far fa-trash-alt"></a></i> --}}
+                    <td>
 
                         <a class="" href="#" onclick="event.preventDefault();
-                                document.getElementById('delete-user{{ $category->id }}').submit();"><i class="far fa-trash-alt">
+                                document.getElementById('delete-category{{ $category->id }}').submit();"><i class="far fa-trash-alt">
                                     </i>
                                 </a>
-                            <form id="delete-user{{ $category->id }}" action="/admin/food-categories/{{ $category->id }}/delete" method="POST" class="d-none">
+                            <form id="delete-category{{ $category->id }}" action="/admin/food-categories/{{ $category->id }}/delete" method="POST" class="d-none">
                                 @method('DELETE')
                                 @csrf
                             </form>
--- resources/views/admin/food-items/all.blade.php
+++ resources/views/admin/food-items/all.blade.php
@@ -8,13 +8,13 @@
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
             <div class="page-header">
-                <h2 class="pageheader-title">Food Items</h2>
+                <h2 class="pageheader-title">Food items</h2>
                 <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                 <div class="page-breadcrumb">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Food Items</li>
+                            <li class="breadcrumb-item active" aria-current="page">Food items</li>
                         </ol>
                     </nav>
                 </div>
@@ -31,30 +31,41 @@
             <!-- ============================================================== -->
             <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
                 <div class="card">
-                    <h5 class="card-header">All Food Items</h5>
+                    <h5 class="card-header">All Food items</h5>
                     <div class="card-body">
-          <table class="table">
-              <thead>
-                  <tr>
-                      <th scope="col">Id</th>
-                      <th scope="col">Title</th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Date Created</th>
-                      <th scope="col">Edit</th>
-                      <th scope="col">Delete</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <th scope="row">1</th>
-                      <td>Burgers</td>
-                      <td>$9</td>
-                      <td>2/2/2020</td>
-                      <td><a href="/admin/food-items/1/edit"><i class="far fa-edit"></i></a></td>
-                      <td><a href="/admin/food-items/1/delete" onclick="if(! confirm('Are you sure you want to delete category?')) { return false; }"><i class="far fa-trash-alt"></a></i></td>
-                  </tr>
-              </tbody>
-          </table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Date</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($items as $item)
+                    <tr>
+                    <th scope="row">{{ $item->id }}</th>
+                    <td>{{ $item->title }}</td>
+                    <td>{{ $item->price }}</td>
+                    <td>{{ date('m/d/Y', strtotime($item->updated_at)) }}</td>
+                    <td><a href="/admin/food-items/{{ $item->id }}/edit"><i class="far fa-edit"></i></a></td>
+                    <td>
+                        <a class="" href="#" onclick="event.preventDefault();
+                                document.getElementById('delete-item{{ $item->id }}').submit();"><i class="far fa-trash-alt">
+                                    </i>
+                                </a>
+                            <form id="delete-item{{ $item->id }}" action="/admin/food-items/{{ $item->id }}/delete" method="POST" class="d-none">
+                                @method('DELETE')
+                                @csrf
+                            </form>
+                    </td>
+                </tr>
+                @endforeach
+        </tbody>
+    </table>
                     </div>
                 </div>
             </div>
