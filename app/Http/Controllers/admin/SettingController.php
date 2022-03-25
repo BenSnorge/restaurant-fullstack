99f32aed104cfc3efd0ea110ef7a2a6ed95c842f
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2022-03-25 13:06:17 +0100

    general settings pages

--- app/Http/Controllers/admin/SettingController.php
+++ app/Http/Controllers/admin/SettingController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\GeneralSetting;
 
 class SettingController extends Controller
 {
-    //
+
+    public function general()
+    {
+        $id = 1;
+        $general_setting = GeneralSetting::find($id);
+        return view('admin/settings/general', [
+            'general_setting' => $general_setting
+        ]);
+    }
+    public function saveGeneral()
+    {
+        $id = 1;
+        request()->validate([
+            'site_title' => ['required', 'string', 'max:255'],
+            'address_1' => ['required', 'string'],
+            /*  'address_2' => ['string'], */
+            'city' => ['required', 'string'],
+            'state' => ['required', 'string'],
+            'zipcode' => ['required', 'string'],
+            'phone_number' => ['required', 'string']
+        ]);
+
+        $general_setting = GeneralSetting::find($id);
+        $general_setting->site_title = request('site_title');
+        $general_setting->address_1 = request('address_1');
+        $general_setting->address_2 = request('address_2');
+        $general_setting->city = request('city');
+        $general_setting->state = request('state');
+        $general_setting->zipcode = request('zipcode');
+        $general_setting->phone_number = request('phone_number');
+        $general_setting->save();
+
+        return redirect('admin/settings/general');
+    }
+    public function delete($id)
+    {
+        $general_setting = GeneralSetting::find($id);
+        $general_setting->delete();
+        return redirect('admin/settings/general');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -67,11 +67,11 @@ Route::delete('/admin/reservations/{id}/delete', 'admin\ReservationController@de
 
 /// ADMIN SETTINGS
 Route::get('/admin/settings/general', 'admin\SettingController@general');
-Route::post('/admin/settings/general', 'admin\SettingController@saveGeneral');
+Route::put('/admin/settings/general', 'admin\SettingController@saveGeneral');
 Route::get('/admin/settings/seo', 'admin\SettingController@seo');
-Route::post('/admin/settings/seo', 'admin\SettingController@saveSeo');
+Route::put('/admin/settings/seo', 'admin\SettingController@saveSeo');
 Route::get('/admin/settings/social', 'admin\SettingController@social');
-Route::post('/admin/settings/social', 'admin\SettingController@saveSocial');
+Route::put('/admin/settings/social', 'admin\SettingController@saveSocial');
 
 
 
