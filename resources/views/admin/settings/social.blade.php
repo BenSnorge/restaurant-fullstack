cc86f3bfef24f06d5d0896fc9141018439098d75
Benjamin Sparwasser <benjaminsparwasser@gmail.com>
2022-03-30 11:31:49 +0200

    seo and socail media settingss

--- resources/views/admin/settings/social.blade.php
+++ resources/views/admin/settings/social.blade.php
@@ -8,13 +8,13 @@
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">Edit  seo Setting </h2>
+                            <h2 class="pageheader-title">Edit  Social Media Setting </h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Edit seo Setting</li>
+            <li class="breadcrumb-item active" aria-current="page">Edit Social Media Setting</li>
         </ol>
     </nav>
                             </div>
@@ -30,26 +30,36 @@
 <!-- ============================================================== -->
 <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
     <div class="card">
-        <h5 class="card-header">Edit seo Setting</h5>
+        <h5 class="card-header">Edit Social Media Setting</h5>
         <div class="card-body">
-      <form method="POST" action="/admin/settings/seo">
+      <form method="POST" action="/admin/settings/social">
           @csrf
           @method('PUT')
           <div class="form-group">
-              <label for="inputdescription">Description</label>
-              <input id="inputdescription" type="text" class="form-control form-control-lg @error('description') is-invalid @enderror" name="description" value="{{ old('description', $seo_setting->description) }}" required autocomplete="description" autofocus placeholder="Add Description">
+              <label for="inputfacebook">Facebook Profile URL</label>
+              <input id="inputfacebook" type="text" class="form-control form-control-lg @error('facebook_url') is-invalid @enderror" name="facebook_url" value="{{ old('facebook_url', $social_setting->facebook_url) }}" required autocomplete="facebook_url" autofocus placeholder="Add Description">
 
-              @error('description')
+              @error('facebook_url')
                   <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
                   </span>
               @enderror
           </div>
           <div class="form-group">
-              <label for="inputkeywords">Keywords</label>
-              <input id="inputkeywords" type="text" class="form-control form-control-lg @error('keywords') is-invalid @enderror" name="keywords" value="{{ old('keywords', $seo_setting->keywords) }}" required autocomplete="keywords" autofocus placeholder="Enter keywords">
+              <label for="inputtwitter">Twitter Profile URL</label>
+              <input id="inputtwitter" type="text" class="form-control form-control-lg @error('twitter_url') is-invalid @enderror" name="twitter_url" value="{{ old('twitter_url', $social_setting->twitter_url) }}" required autocomplete="twitter_url" autofocus placeholder="Add Description">
 
-              @error('keywords')
+              @error('twitter_url')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+          </div>
+          <div class="form-group">
+              <label for="inputinstagram">Instagram Profile URL</label>
+              <input id="inputinstagram" type="text" class="form-control form-control-lg @error('instagram_url') is-invalid @enderror" name="instagram_url" value="{{ old('instagram_url', $social_setting->instagram_url) }}" required autocomplete="instagram_url" autofocus placeholder="Add Description">
+
+              @error('instagram_url')
                   <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
                   </span>
